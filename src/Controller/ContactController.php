11ee56ae42ea75c3f05c12821f057039d4f51217
Vega <aziaha.formations@gmail.com>
2026-01-13 14:09:17 +0100

    Affichage du formulaire d'ajout de contact  en cours

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -22,9 +23,11 @@ final class ContactController extends AbstractController
 
         // 2a. Créer le type du formulaire
         // 2b. L'utiliser pour créer le formulaire
-        
+        $form = $this->createForm(ContactFormType::class, $contact);
 
         // 3. Passer la partie visible du formulaire à la vue pour affichage
-        return $this->render('contact/create.html.twig');
+        return $this->render('contact/create.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
