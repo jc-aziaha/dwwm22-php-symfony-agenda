2f2c7007e98ddba8a424f97efda1dcb5d11cd1ba
Vega <aziaha.formations@gmail.com>
2026-01-13 12:51:53 +0100

    Création du contrôleur et du type du formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ContactController extends AbstractController
 {
     #[Route('/', name: 'app_contact_index', methods: ['GET'])]
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('contact/index.html.twig');
+    }
+
+    #[Route('/contact/create', name: 'app_contact_create', methods: ['GET'])]
+    public function create(): Response {
+
+        // 1. Créer le contact à insérer en base de données.
+        $contact = new Contact();
+
+        // 2a. Créer le type du formulaire
+        // 2b. L'utiliser pour créer le formulaire
+        
+
+        // 3. Passer la partie visible du formulaire à la vue pour affichage
+        return $this->render('contact/create.html.twig');
     }
 }
